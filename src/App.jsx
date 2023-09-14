b33298d7b1ecb306d29f1b9f0657d3eea04c20bd
diegocallegari96 <warsindustry@gmail.com>
2023-09-14 13:35:28 +0200

    light button assignment

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const [light, setLight] = React.useState(false)
+    const toggleLight = () => {
+        setLight(!light);
+    };
+    console.log(light)
+
     return (
-        <main className="off">
+        <main className={light === true ? "on" : "off"}>
             <section>
                 <div className="dot"></div>
-                <button type="button">Turn on/off</button>
+                <button type="button"
+                        onClick={toggleLight}
+                >{light === true ? "Turn off" : "Turn on"}</button>
             </section>
         </main>
     );
